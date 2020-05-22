72a2e82a3fdd9121b63621ca15cb01511a796e5e
Philipp <30511472+p-fruck@users.noreply.github.com>
2020-05-22 15:39:12 +0200

    :zap: Moved classes to interfaces to reduce overhead

---
+++ packages/backend/src/controllers/moodle/course.interface.ts
@@ -0,0 +1,9 @@
+import { IAssignment } from './assignment.interface';
+
+export interface ICourse {
+    id: number;
+    fullname: string;
+    shortname: string;
+    timemodified: number;
+    assignments: IAssignment[];
+}
