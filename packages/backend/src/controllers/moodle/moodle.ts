1afb6f9f4a8f8dbf85a64657bc834f52aea8cb3d
Philipp <30511472+p-fruck@users.noreply.github.com>
2020-05-25 16:55:09 +0200

    :ok_hand: Added JSDocs

--- packages/backend/src/controllers/moodle/moodle.ts
+++ packages/backend/src/controllers/moodle/moodle.ts
@@ -6,7 +6,13 @@ import { IRessource } from './ressource.interface';
 import { ICourseDetails } from './coursedetails.interface';
 import { LastFetch } from './lastfetch.schema';
 
-function getBaseUrl() {
+/**
+ * Builds a string representing the moodle 
+ * base url that web requests are being send to
+ * 
+ * @returns {string} The base url
+ */
+function getBaseUrl(): string {
     let url = config.moodle.baseURL;
     if (!url.endsWith('/')) {
         url += '/';
@@ -16,19 +22,27 @@ function getBaseUrl() {
                 + '&moodlewsrestformat=json';
 }
 
+/**
+ * Reads timestamp of the last fetch from database
+ * (Creates a new one if this is the first fetch)
+ * 
+ * @returns {number} The timestamp of last fetch (in seconds!) or
+ *                   the current Date if this is the first fetch
+ */
 async function getLastFetch(): Promise<number> {
-    const date = Math.floor(Date.now() / 1000);
-    return LastFetch.findOneAndUpdate({}, {$set: {timestamp: date}})
-            .then(query => query == null ? new LastFetch({timestamp: date}).save() : query)
+    const now = Math.floor(Date.now() / 1000);
+    return LastFetch.findOneAndUpdate({}, {$set: {timestamp: now}})
+            .then(query => query == null ? new LastFetch({timestamp: now}).save() : query)
             .then(query => query.timestamp);
 }
 
 /**
- * Fetches all the data from the Moodle webservice
+ * Fetches all data from the Moodle webservice, filters it
+ * and notifies about newly created or updated ressources
  *
  * @export
  */
-export function fetchMoodleData() {
+export function fetchMoodleData(): void {
     const moodleUrl = getBaseUrl();
     // TODO: Replace placeholder with real blacklist
     const courseBlacklist: number[] = [];
@@ -44,24 +58,42 @@ export function fetchMoodleData() {
     });
 }
 
+/**
+ * Fetches all Assignments from the moodle instance
+ * 
+ * @param {string} moodleUrl - Moodle Web Service Url
+ * @returns {Promise<ICourse[]>} A Promise of an array of Courses containing the Assignments
+ */
 async function fetchAssignments(moodleUrl: string): Promise<ICourse[]> {
     return fetch(moodleUrl + '&wsfunction=mod_assign_get_assignments')
-	  .then(res => res.json())
+      .then(res => res.json())
       .then(json => json.courses)
     .catch((error) => {
         loggerFile.error('Moodle API request failed', error);
     });
 }
 
+/**
+ * Fetches all Ressources (Files etc) from the moodle instance
+ * 
+ * @param {string} moodleUrl - Moodle Web Service Url
+ * @returns {Promise<IRessource[]>} A Promise of an array of Ressources
+ */
 async function fetchRessources(moodleUrl: string): Promise<IRessource[]> {
     return fetch(moodleUrl + '&wsfunction=mod_resource_get_resources_by_courses')
-	  .then(res => res.json())
+      .then(res => res.json())
       .then(json => json.resources)
     .catch((error) => {
         loggerFile.error('Moodle API request failed', error);
     });
 }
 
+/**
+ * Fetches all Courses (CourseDetails) from the moodle instance
+ * 
+ * @param {string} moodleUrl - Moodle Web Service Url
+ * @returns {Promise<ICourseDetails[]>} A Promise of an array of CourseDetails
+ */
 async function fetchEnroledCourses(moodleUrl: string): Promise<ICourseDetails[]> {
     return fetch(moodleUrl + '&wsfunction=core_enrol_get_users_courses&userid='+config.moodle.userId)
 	  .then(res => res.json())
@@ -70,7 +102,13 @@ async function fetchEnroledCourses(moodleUrl: string): Promise<ICourseDetails[]>
     });
 }
 
-function printNewAssignments(courses: ICourse[], lastFetch: number):void {
+/**
+ * Filters assignments by timestamp and notifies about changes
+ * 
+ * @param {ICourse[]} courses - The courses containing the Assignments
+ * @param {number} lastFetch - The timestamp of the last fetch (in seconds!)
+ */
+function printNewAssignments(courses: ICourse[], lastFetch: number): void {
     courses.forEach(course => {
         course.assignments.forEach( assignment => {
             if (assignment.timemodified > lastFetch) {
@@ -81,7 +119,13 @@ function printNewAssignments(courses: ICourse[], lastFetch: number):void {
     });
 }
 
-function printNewRessources(ressources: IRessource[], moodleUrl: string, lastFetch: number):void {
+/**
+ * Filters Ressources by timestamp and notifies about changes
+ * 
+ * @param {ICourse[]} courses - The Ressources to filter
+ * @param {number} lastFetch - The timestamp of the last fetch (in seconds!)
+ */
+function printNewRessources(ressources: IRessource[], moodleUrl: string, lastFetch: number): void {
     if (ressources.length === 0) { return; }
     const courseMap: Map<number, string> = new Map();
 
