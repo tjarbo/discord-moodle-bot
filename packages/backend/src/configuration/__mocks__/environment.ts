197b5a428524168f1aa75a0f510a5beaf201caff
tjarbo <16938041+tjarbo@users.noreply.github.com>
2020-05-27 14:52:53 +0200

    :bug: Fixed unit tests

---
+++ packages/backend/src/configuration/__mocks__/environment.ts
@@ -0,0 +1,18 @@
+export const config = {
+  admin: {
+    id: "1234567890123456789",
+    name: "testuser#00000",
+  },
+  discordToken: "xxxxxxxxxxxxxx",
+  env: "test",
+  jwt: {
+    secret: "secret",
+    expiresIn: "6h",
+  },
+  mongo: {
+    host: "mongodb://localhost:27017/fmdb",
+    port: 27017
+  },
+  mongooseDebug: true,
+  port: 4040,
+};
