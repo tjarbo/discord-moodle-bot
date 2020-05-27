20a13007f18f287a16ab3da5e967a6a4f2b114ab
Philipp <30511472+p-fruck@users.noreply.github.com>
2020-05-27 18:57:10 +0200

    :construction: Working on unit-tests

--- packages/backend/tests/moodle.spec.ts
+++ packages/backend/tests/moodle.spec.ts
@@ -1,4 +1,19 @@
 import * as moodle from '../src/controllers/moodle/moodle'; 
+import mockingoose from 'mockingoose';
+import fetch from 'node-fetch';
+import { mocked } from 'ts-jest/utils';
+import { ICourse } from '../src/controllers/moodle/course.interface';
+import { IRessource } from '../src/controllers/moodle/ressource.interface';
+import { loggerFile } from '../src/configuration/logger';
+import { LastFetch } from '../src/controllers/moodle/lastfetch.schema';
+
+jest.mock('node-fetch', () => {
+    return jest.fn();
+  });
+  
+ /* beforeEach(() => {
+    mocked(fetch).mockClear();
+  });*/
 
 jest.mock('../src/configuration/environment', () => ({
     config: {
@@ -9,55 +24,127 @@ jest.mock('../src/configuration/environment', () => ({
     },
 }));
 
-
 describe('getBaseUrl', () => {
     it('should build a valid moodle url', () => {       
         let url = 'https://moodle.example.com/webservice/rest/server.php?wstoken=MOODLETOKEN123&moodlewsrestformat=json';
         expect(moodle.getBaseUrl()).toBe(url);
-    })        
+    })
 });
 
 describe('getLastFetch', () => {
-    it('should return the timestamp of the last fetch stored in mongodb', () => {
-
-    })
-
-    it ('should create a new timestamp and return the current date if this is the first fetch', () => {
-
-    })
-});
-
-describe('fetchMoodleData', () => {
-    it('Only print new Assignments that aren´t blacklisted', () => {
-        
-    })
+    let spyLastFetchSave: jest.SpyInstance;
     
-    it('Only print new Ressources that aren´t blacklisted', () => {
+    beforeEach(() => {
+        spyLastFetchSave = jest.spyOn(new LastFetch(), 'save');
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should return the timestamp of the last fetch stored in mongodb', async () => {
+        mockingoose(LastFetch).toReturn({timestamp: 123}, 'findOneAndUpdate');
+
+        expect(await moodle.getLastFetch()).toBe(123);
+        expect(spyLastFetchSave.mock.calls.length).toBe(0);
+     })
+
+    it ('should create a new timestamp and return the current date if this is the first fetch', async () => {
+        mockingoose(LastFetch).toReturn(() => null, 'findOneAndUpdate');
+        let currentTime = Math.floor(Date.now() / 1000);
         
+        expect(await moodle.getLastFetch()).toBeGreaterThanOrEqual(currentTime);
+        expect(spyLastFetchSave.mock.calls.length).toBe(1);
+    })
+
+});
+/*
+describe('fetchMoodleData', () => {
+    let spyGetLastFetch: jest.SpyInstance;
+    let spyFetchAssignments: jest.SpyInstance;
+    let spyFetchRessources: jest.SpyInstance;
+    let spyPrintNewAssignments: jest.SpyInstance;
+    let spyPrintNewRessources: jest.SpyInstance;
+
+    beforeEach(() => {
+        spyGetLastFetch = jest.spyOn(moodle, 'getLastFetch');
+        spyGetLastFetch.mockResolvedValue(123);
+
+        let mockAssignments: ICourse[] = [{id: 0, assignments: []}, {id: 1, assignments: []}] as ICourse[];
+        spyFetchAssignments = jest.spyOn(moodle, 'fetchAssignments');
+        spyFetchAssignments.mockResolvedValue(mockAssignments);
+
+        let mockRessources: IRessource[] = [{course: 0}, {course: 1}] as IRessource[];
+        spyFetchRessources = jest.spyOn(moodle, 'fetchRessources');
+        spyFetchRessources.mockResolvedValue(mockRessources);
+
+        spyPrintNewAssignments = jest.spyOn(moodle, 'printNewAssignments');
+        spyPrintNewRessources = jest.spyOn(moodle, 'printNewRessources');
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should only print new Ressources and Assignments that aren´t blacklisted', async () => {
+        let data = await moodle.fetchMoodleData();
+        console.log(data);
+        expect(spyFetchAssignments.mock.calls.length).toBe(1);
+        expect(spyFetchRessources).toHaveBeenCalledTimes(1);
     })
 });
-
+*/
 describe('fetchAssignments', () => {
-    it('should return an array of all courses containing the assignments provided by the moodle API', () => {
-        
+    let spyLogger: jest.SpyInstance;
+
+    beforeEach(() => {
+        spyLogger = jest.spyOn(loggerFile, 'error');
+    });
+    
+    afterEach(() => {
+        jest.resetAllMocks();
     })
 
-    it('should throw an error if the request fails', () => {
+    it('should return an array of all courses containing the assignments provided by the moodle API', async () => {
+        mocked(fetch).mockImplementationOnce((): Promise<any> => Promise.resolve({
+            json: () => ({courses: []})
+        }))
+        expect(await moodle.fetchAssignments(null)).toStrictEqual([]);
+    })
 
+    it('should throw an error if the request fails', async () => {
+        mocked(fetch).mockImplementationOnce((): Promise<any> => Promise.reject());
+        await moodle.fetchAssignments(null);
+        expect(spyLogger).toHaveBeenCalledTimes(1);
     })
 });
 
 describe('fetchRessources', () => {
-    it('should return an array of all ressources provided by the moodle API', () => {
-        
+    let spyLogger: jest.SpyInstance;
+    
+    beforeEach(() => {
+        spyLogger = jest.spyOn(loggerFile, 'error');
+    });
+    
+    afterEach(() => {
+        jest.resetAllMocks();
     })
 
-    it('should throw an error if the request fails', () => {
+    it('should return an array of all ressources provided by the moodle API', async () => {
+        mocked(fetch).mockImplementationOnce((): Promise<any> => Promise.resolve({
+            json: () => ({resources: []})
+        }));
+        expect(await moodle.fetchRessources(null)).toStrictEqual([]);
+    })
 
-    })      
+    it('should throw an error if the request fails', async () => {
+        mocked(fetch).mockImplementationOnce((): Promise<any> => Promise.reject());
+        await moodle.fetchRessources(null);
+        expect(spyLogger).toHaveBeenCalledTimes(1);
+    })
 });
 
-describe('fetchEnroledCourses', () => {
+describe('fetchEnrolledCourses', () => {
     it('should return an array of all ressources provided by the moodle API', () => {
         
     })
