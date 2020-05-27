a584aa56319d21669366841ac72bcb93b7e12430
Philipp <30511472+p-fruck@users.noreply.github.com>
2020-05-27 23:53:46 +0200

    :construction: Working on unit-tests

--- packages/backend/src/configuration/__mocks__/environment.ts
+++ packages/backend/src/configuration/__mocks__/environment.ts
@@ -1,18 +1,23 @@
 export const config = {
-  admin: {
-    id: '1234567890123456789',
-    name: 'testuser#00000',
-  },
-  discordToken: 'xxxxxxxxxxxxxx',
-  env: 'test',
-  jwt: {
-    secret: 'secret',
-    expiresIn: '6h',
-  },
-  mongo: {
-    host: 'mongodb://localhost:27017/fmdb',
-    port: 27017
-  },
-  mongooseDebug: true,
-  port: 4040,
-};
+    admin: {
+      id: '1234567890123456789',
+      name: 'testuser#00000',
+    },
+    discordToken: 'xxxxxxxxxxxxxx',
+    env: 'test',
+    jwt: {
+      secret: 'secret',
+      expiresIn: '6h',
+    },
+    mongo: {
+      host: 'mongodb://localhost:27017/fmdb',
+      port: 27017
+    },
+    mongooseDebug: true,
+    port: 4040,
+    moodle: {
+        baseURL: "https://moodle.example.com",
+        token:   "MOODLETOKEN123",
+        userId: 123456
+    },
+  };
