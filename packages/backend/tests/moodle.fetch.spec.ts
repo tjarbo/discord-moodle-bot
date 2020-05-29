90d932313cbee42cd9fcc3253889413108bdffa4
tjarbo <16938041+tjarbo@users.noreply.github.com>
2020-05-29 19:22:45 +0200

    :art: Small changes

--- packages/backend/tests/moodle.fetch.spec.ts
+++ packages/backend/tests/moodle.fetch.spec.ts
@@ -14,89 +14,89 @@ const mockFetch = (res: any) =>
 const mockFailedFetch = () =>
     mocked(fetch).mockImplementationOnce(() => Promise.reject());
 
-    describe('fetchAssignments', () => {
-        let spyLogger: jest.SpyInstance;
-    
-        beforeEach(() => {
-            spyLogger = jest.spyOn(loggerFile, 'error');
-        });
-    
-        afterEach(() => {
-            jest.resetAllMocks();
-        });
-    
-        it('should address the correct moodle wsfunction', async () => {
-            mockFailedFetch();
-            await moodle.fetchAssignments('');
-            expect(mocked(fetch)).toHaveBeenCalledWith('&wsfunction=mod_assign_get_assignments');
-        });
-    
-        it('should return an array of all courses containing the assignments provided by the moodle API', async () => {
-            mockFetch({courses: []});
-            expect(await moodle.fetchAssignments('')).toStrictEqual([]);
-        });
-    
-        it('should throw an error if the request fails', async () => {
-            mockFailedFetch();
-            await moodle.fetchAssignments('');
-            expect(spyLogger).toHaveBeenCalledTimes(1);
-        });
+describe('fetchAssignments', () => {
+    let spyLogger: jest.SpyInstance;
+
+    beforeEach(() => {
+        spyLogger = jest.spyOn(loggerFile, 'error');
     });
-    
-    describe('fetchRessources', () => {
-        let spyLogger: jest.SpyInstance;
-    
-        beforeEach(() => {
-            spyLogger = jest.spyOn(loggerFile, 'error');
-        });
-    
-        afterEach(() => {
-            jest.resetAllMocks();
-        });
-    
-        it('should address the correct moodle wsfunction', async () => {
-            mockFailedFetch();
-            await moodle.fetchRessources('');
-            expect(mocked(fetch)).toHaveBeenCalledWith('&wsfunction=mod_resource_get_resources_by_courses');
-        });
-    
-        it('should return an array of all ressources provided by the moodle API', async () => {
-            mockFetch({resources: []});
-            expect(await moodle.fetchRessources('')).toStrictEqual([]);
-        });
-    
-        it('should throw an error if the request fails', async () => {
-            mockFailedFetch();
-            await moodle.fetchRessources('');
-            expect(spyLogger).toHaveBeenCalledTimes(1);
-        });
+
+    afterEach(() => {
+        jest.resetAllMocks();
     });
-    
-    describe('fetchEnrolledCourses', () => {
-        let spyLogger: jest.SpyInstance;
-    
-        beforeEach(() => {
-            spyLogger = jest.spyOn(loggerFile, 'error');
-        });
-    
-        afterEach(() => {
-            jest.resetAllMocks();
-        });
-    
-        it('should address the correct moodle wsfunction', async () => {
-            mockFailedFetch();
-            await moodle.fetchEnrolledCourses('');
-            expect(mocked(fetch)).toHaveBeenCalledWith('&wsfunction=core_enrol_get_users_courses&userid=123456');
-        });
-    
-        it('should return an array of all ressources provided by the moodle API', async () => {
-            mockFetch([]);
-            expect(await moodle.fetchEnrolledCourses('')).toStrictEqual([]);
-        });
-    
-        it('should throw an error if the request fails', async () => {
-            mockFailedFetch();
-            await moodle.fetchEnrolledCourses('');
-            expect(spyLogger).toHaveBeenCalledTimes(1);
-        });
+
+    it('should address the correct moodle wsfunction', async () => {
+        mockFailedFetch();
+        await moodle.fetchAssignments('');
+        expect(mocked(fetch)).toHaveBeenCalledWith('&wsfunction=mod_assign_get_assignments');
     });
+
+    it('should return an array of all courses containing the assignments provided by the moodle API', async () => {
+        mockFetch({ courses: [] });
+        expect(await moodle.fetchAssignments('')).toStrictEqual([]);
+    });
+
+    it('should throw an error if the request fails', async () => {
+        mockFailedFetch();
+        await moodle.fetchAssignments('');
+        expect(spyLogger).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('fetchRessources', () => {
+    let spyLogger: jest.SpyInstance;
+
+    beforeEach(() => {
+        spyLogger = jest.spyOn(loggerFile, 'error');
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should address the correct moodle wsfunction', async () => {
+        mockFailedFetch();
+        await moodle.fetchRessources('');
+        expect(mocked(fetch)).toHaveBeenCalledWith('&wsfunction=mod_resource_get_resources_by_courses');
+    });
+
+    it('should return an array of all ressources provided by the moodle API', async () => {
+        mockFetch({ resources: [] });
+        expect(await moodle.fetchRessources('')).toStrictEqual([]);
+    });
+
+    it('should throw an error if the request fails', async () => {
+        mockFailedFetch();
+        await moodle.fetchRessources('');
+        expect(spyLogger).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('fetchEnrolledCourses', () => {
+    let spyLogger: jest.SpyInstance;
+
+    beforeEach(() => {
+        spyLogger = jest.spyOn(loggerFile, 'error');
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should address the correct moodle wsfunction', async () => {
+        mockFailedFetch();
+        await moodle.fetchEnrolledCourses('');
+        expect(mocked(fetch)).toHaveBeenCalledWith('&wsfunction=core_enrol_get_users_courses&userid=123456');
+    });
+
+    it('should return an array of all ressources provided by the moodle API', async () => {
+        mockFetch([]);
+        expect(await moodle.fetchEnrolledCourses('')).toStrictEqual([]);
+    });
+
+    it('should log error if the request fails', async () => {
+        mockFailedFetch();
+        await moodle.fetchEnrolledCourses('');
+        expect(spyLogger).toHaveBeenCalledTimes(1);
+    });
+});
