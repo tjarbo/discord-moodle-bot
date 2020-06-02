055b235a7a6cc6c7452784dfe520c9d047d15938
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2020-06-02 22:23:17 +0200

    :sparkles: Added new feature for setting the moodle refresh rate (#7)
    Changes because of rebase
    
    
    :sparkles: Added middleware, routes and changed format to milliseconds
    
    
    Adjusted API error code
    
    
    Changed request content to body
    
    
    :sparkles: Added frontend dashboard view
    
    
    Minor corrections
    
    
    Added colors to result message
    
    
    Minor corrections
    
    
    Added user input validation with hapi/joi
    
    
    Minor fixes
    
    
    Minor fixes
    
    
    Typo fixed
    
    
    Refactoring caused by rebase
    
    Co-authored-by: Anton <anton.plagemann+github@gmail.com>

--- packages/frontend/src/router/index.js
+++ packages/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Login from '../views/Login.vue';
+import Dashboard from '../views/Dashboard.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Anmeldung',
     component: Login,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
   },
 ];
 
---
+++ packages/frontend/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="dashboardview">
+      <RefreshRateInput></RefreshRateInput>
+    </div>
+</template>
+
+<script>
+import RefreshRateInput from '../components/RefreshRateInput.vue';
+
+export default {
+  name: 'DashboardView',
+  components: { RefreshRateInput },
+};
+</script>
+
+<style scoped>
+
+</style>
