da42f8851239295c9d16e68e15d7a4ed5510873e
Anton <anton.plagemann+github@gmail.com>
2020-06-03 14:43:48 +0200

    Moved notice

--- packages/backend/src/configuration/environment.ts
+++ packages/backend/src/configuration/environment.ts
@@ -51,11 +51,6 @@ const envVarsSchema = object({
 const { error, value: envVars } = envVarsSchema.validate(process.env);
 if (error) throw new Error(`Config validation error: ${error.message}`);
 
-/**
- * Attention: For coding use get functions first if defined!
- * Example: getDiscordChannel() and getRefreshRate() instead of
- * constants discordChannel and fetchInterval.
- */
 export const config = {
   admin: {
     id: envVars.ADMIN_ID,
