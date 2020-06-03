0377c697447387c476145596d09b279d0ef28da7
tjarbo <16938041+tjarbo@users.noreply.github.com>
2020-06-03 14:17:00 +0200

    :green_heart: Fixed unit tests for ci part 2

--- packages/frontend/tests/unit/dashboard.view.spec.js
+++ packages/frontend/tests/unit/dashboard.view.spec.js
@@ -1,15 +1,44 @@
 import { expect } from 'chai';
-import { mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
 import Dashboard from '@/views/Dashboard.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
 
 describe('Dashboard.view', () => {
   let wrapper = null;
+  let getters;
+  let store;
 
   beforeEach(() => {
-    wrapper = mount(Dashboard);
+    getters = {
+      authGetError: () => null,
+      authGetStatus: () => ({
+        fail: false,
+      }),
+      administratorGetStatus: () => ({
+        fail: false,
+      }),
+    };
+
+    store = new Vuex.Store({
+      getters,
+    });
+
+    wrapper = mount(Dashboard, { store, localVue });
   });
 
-  it('renders a navbar', () => {
+  it('should render a navbar', () => {
     expect(wrapper.contains('#navbar')).is.true;
+  });
+
+  it('should render \'Add Administrator\'', () => {
+    expect(wrapper.contains('#addadministrator')).is.true;
+  });
+
+  it('should render \'set Refreshrate\'', () => {
+    expect(wrapper.contains('#refreshRateInput')).is.true;
   });
 });
