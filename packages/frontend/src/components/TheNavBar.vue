81b3b9ecabca6257f550c20e3fecc891621e96da
tjarbo <16938041+tjarbo@users.noreply.github.com>
2020-06-29 21:24:29 +0200

    :lipstick: Bulma for NavBar and Dashboard

---
+++ packages/frontend/src/components/TheNavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item has-text-weight-bold">
+        <img src="../assets/FMDB_logo.png" height="auto">
+      </div>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary" @click="onClick">
+              <strong>Logout</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    onClick() {
+      this.$store.dispatch('logout');
+      this.$router.push('/');
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
--- packages/frontend/src/views/Dashboard.vue
+++ packages/frontend/src/views/Dashboard.vue
@@ -1,16 +1,28 @@
 <template>
-    <div id="dashboardview">
-      <NavBar></NavBar>
-      <RefreshRateInput></RefreshRateInput>
-      <DiscordChannelInput></DiscordChannelInput>
-      <CourseSelection></CourseSelection>
-      <AddAdministrator></AddAdministrator>
+  <div id="dashboardview">
+    <TheNavBar></TheNavBar>
+    <div class="columns">
+      <div class="column is-10 is-offset-1">
+        <b-tabs position="is-centered" class="block">
+          <b-tab-item label="Administration">
+            <add-administrator></add-administrator>
+          </b-tab-item>
+          <b-tab-item label="Moodle">
+            <course-selection></course-selection>
+            <refresh-rate-input></refresh-rate-input>
+          </b-tab-item>
+          <b-tab-item label="Discord">
+            <discord-channel-input></discord-channel-input>
+          </b-tab-item>
+        </b-tabs>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import RefreshRateInput from '../components/RefreshRateInput.vue';
-import NavBar from '../components/NavBar.vue';
+import TheNavBar from '../components/TheNavBar.vue';
 import CourseSelection from '../components/CourseSelection.vue';
 import DiscordChannelInput from '../components/DiscordChannelInput.vue';
 import AddAdministrator from '../components/AddAdministrator.vue';
@@ -19,7 +31,7 @@ export default {
   name: 'DashboardView',
   components: {
     RefreshRateInput,
-    NavBar,
+    TheNavBar,
     AddAdministrator,
     CourseSelection,
     DiscordChannelInput,
@@ -28,5 +40,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
