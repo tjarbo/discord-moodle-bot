69dc6e7e711dc74ed1f584de0f085079e0c40376
Tjark <16938041+tjarbo@users.noreply.github.com>
2020-11-17 12:08:48 +0100

    🔀 Überprüfung der Moodle-API unter fmdb.tjarbo.me (#69)
    * ✨ Überprüfung der Moodle-API unter fmdb.tjarbo.me
    Fixes #68
    
    * :pencil2: Fixed typo

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,11 @@
 
-const usernameInput = document.getElementById('username');
+const usernameInput  = document.getElementById('username');
 const moodleURLInput = document.getElementById('moodleurl');
-const passwordInput = document.getElementById('password');
-const submitButton = document.getElementById('submit');
-const form = document.getElementById('form');
-const modal = document.getElementById('modal');
+const passwordInput  = document.getElementById('password');
+const submitButton   = document.getElementById('submit');
+const form           = document.getElementById('form');
+const modal          = document.getElementById('modal');
+const errorMessage   = document.getElementById('errormessage');
 
 /**
  * Enable or disable input fields
@@ -24,47 +25,13 @@ function setLoading(on) {
 }
 
 /**
- * Sends a request to the submitted moodle service to
- * request a token to access the api passwordless
- * 
- * @returns {Promise} Request
+ * Send a request to the url and returns it as a object.
+ *
+ * @param {string} url request url
+ * @return {Promise} JSON-Object
  */
-function requestToken() {
-
-    const url = [
-        moodleURLInput.value,
-        '/login/token.php',
-        '?username=',
-        usernameInput.value,
-        '&password=',
-        passwordInput.value,
-        '&service=moodle_mobile_app'
-    ].join('')
-
-
-    return fetch(url).then(response => response.json())
-}
-
-/**
- * Sends a request to the moodle service to get basic information about the moodle service
- * and the user
- * 
- * @param {string} token Token from successful requestToken()
- * @returns {Promise} Request
- */
-function requestPersonalId(token) {
-    
-    const url = [
-        moodleURLInput.value,
-        '/webservice/rest/server.php',
-        '?wsfunction=core_webservice_get_site_info',
-        '&wstoken=',
-        token,
-        '&moodlewsrestformat=json'
-    ].join('')
-
-
-    return fetch(url).then(response => response.json())
+function requestJSON(url) {
+    return fetch(url).then(response => response.json()) ;   
 }
 
 /**
@@ -76,7 +43,7 @@ function requestPersonalId(token) {
  * @param {string} token Token to use the moodle api
  * @param {string} userid Userid of the user
  */
-function showResult(firstname, university, moodlebaseurl ,token, userid) {
+function showResult(firstname, university, moodlebaseurl, token, userid) {
     const firstnameSpan     = document.getElementById('firstname');
     const universitySpan    = document.getElementById('university');
     const tokenSpan         = document.getElementById('token');
@@ -90,12 +57,87 @@ function showResult(firstname, university, moodlebaseurl ,token, userid) {
     moodlebaseurlSpan.innerText = moodlebaseurl;
 
     modal.classList.add('is-active');
-    document.getElementById('close').onclick = () => { modal.classList.remove('is-active'); }
+    document.getElementById('close').onclick = () => { 
+        modal.classList.remove('is-active'); 
+        errorMessage.classList.add('is-hidden');
+    }
 }
 
+/**
+ * Displays the error message
+ *
+ * @param {*} text Description
+ * @param {*} response Response from the api
+ * @param {*} url Url that has been requested
+ */
+function showApiError(text, response, url) {
+    const errorTextSpan     = document.getElementById('errortext');
+    const errorRequestSpan  = document.getElementById('errorrequest');
+    const errorResponseSpan = document.getElementById('errorresponse');
+
+    errorTextSpan.innerText     = text;
+    errorRequestSpan.innerText  = url;
+    errorResponseSpan.innerText = response;
+
+    errorMessage.classList.remove('is-hidden');
+}
+
+/**
+ * Performs a demo request to check, if the moodle-api is reachable and
+ * works.
+ *
+ * @param {*} moodlebaseurl URL to access the moodle instance
+ * @param {*} token Token to use the moodle api
+ * @param {*} userid Userid of the user
+ */
+async function checkApi(moodlebaseurl, token, userid) {
+    const url = [
+        moodlebaseurl,
+        '/webservice/rest/server.php',
+        '?wsfunction=core_enrol_get_users_courses',
+        '&userid=',
+        userid,
+        '&wstoken=',
+        token,
+        '&moodlewsrestformat=json'
+    ].join('');
+
+    console.info("Überprüfe Moodle-API funktionalität", url);
+
+    try {
+        let response = await requestJSON(url);
+        console.log("** Antwort", response)
+        
+        // Something went wrong -> received moodle error
+        if ('errorcode' in response) {
+
+            const text = `Beim Überprüfen der Moodle Schnittstellen ist ein Fehler aufgetreten. Dann kann verschiedene Gründe haben,
+            eventuell können dir dabei aber nur die Administratoren deiner Moodle-Platform weiterhelfen. Details
+            zu der Fehlermeldung findest du unten. Leider kann der Bot ohne diese Schnittstelle nicht korrekt
+            funktionieren.
+            `;
+
+            showApiError(text, JSON.stringify(response), url);
+        }
+
+    } catch (error) {
+        console.error("** error", error);
+        // two cases: HttpError -> MoodleError
+        const text = `Beim Überprüfen der Moodle Schnittstellen ist ein Fehler aufgetreten. Das kann entweder an einer
+        veralteten Moodle-Instanz liegen, da die Schnittstelle dort nicht verfügbar ist oder es gab ein Problem mit deiner
+        Internet Verbindung - versuche es nochmal!`;
+        showApiError(text, error.message, url);
+    }
+}
 
 /**
  * Main function to handle and manage the token and environment variables.
+ * 
+ * Sends a request to the submitted moodle service to
+ * request a token to access the api passwordless
+ * 
+ * Sends a request to the moodle service to get basic information about the moodle service
+ * and the user
  * 
  * @param {Event} event 
  */
@@ -111,24 +153,44 @@ async function onSubmit(event) {
     setLoading(true);
 
     try {
-        let response = await requestToken()
+        const urlToken = [
+            moodleURLInput.value,
+            '/login/token.php',
+            '?username=',
+            usernameInput.value,
+            '&password=',
+            passwordInput.value,
+            '&service=moodle_mobile_app'
+        ].join('');
+
+        let response = await requestJSON(urlToken);
         
         if ('token' in response) { token = response.token }
         else if ('error' in response) { throw Error(response.error) }
         else { throw new Error('Unexpected server response') }
 
-        response = await requestPersonalId(token);
+        const urlPersonalId = [
+            moodleURLInput.value,
+            '/webservice/rest/server.php',
+            '?wsfunction=core_webservice_get_site_info',
+            '&wstoken=',
+            token,
+            '&moodlewsrestformat=json'
+        ].join('');
+
+        response = await requestJSON(urlPersonalId);
         console.log(response);
         if ('userid' in response) { 
-            firstname  = response.firstname;
-            university = response.sitename;
-            userid     = response.userid;
+            firstname     = response.firstname;
+            university    = response.sitename;
+            userid        = response.userid;
             moodlebaseurl = response.siteurl;
         }
         else if ('errorcode' in response) { throw new Error(response.message) }
         else { throw new Error('Unexpected server response') }
         
         showResult(firstname, university, moodlebaseurl, token, userid);
+        checkApi(moodlebaseurl, token, userid);
 
     } catch (error) {
         console.error(error);
