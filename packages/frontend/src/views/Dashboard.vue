afc46619e38b6b1986ab63feda5d6c4663c0b684
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-04-17 10:10:58 +0000

    :triangular_flag_on_post: Disabled administrator control #103

--- packages/frontend/src/views/Dashboard.vue
+++ packages/frontend/src/views/Dashboard.vue
@@ -7,10 +7,12 @@
           <b-tab-item class="spacing" label="Statusboard">
             <Status></Status>
           </b-tab-item>
+          <!--
           <b-tab-item class="spacing" label="Administration">
             <AdministratorList></AdministratorList>
             <AddAdministrator></AddAdministrator>
           </b-tab-item>
+          -->
           <b-tab-item class="spacing is-moodle" label="Moodle">
             <SetCourseNotifications></SetCourseNotifications>
             <SetRefreshRate></SetRefreshRate>
@@ -25,8 +27,8 @@
 </template>
 
 <script>
-import AddAdministrator from '../components/administration/AddAdministrator.vue';
-import AdministratorList from '../components/administration/AdministratorList.vue';
+// import AddAdministrator from '../components/administration/AddAdministrator.vue';
+// import AdministratorList from '../components/administration/AdministratorList.vue';
 import SetCourseNotifications from '../components/moodle/SetCourseNotifications.vue';
 import DiscordPanel from '../components/discord/DiscordPanel.vue';
 import SetRefreshRate from '../components/moodle/SetRefreshRate.vue';
@@ -36,8 +38,8 @@ import Status from '../components/statusboard/Status.vue';
 export default {
   name: 'DashboardView',
   components: {
-    AddAdministrator,
-    AdministratorList,
+    //    AddAdministrator,
+    //    AdministratorList,
     SetCourseNotifications,
     DiscordPanel,
     SetRefreshRate,
