f0f4e19181dca1daada8fdf8f0478586198e943a
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-04-27 13:07:32 +0200

    🍻 Updated vue store

--- packages/frontend/src/store/utils/StoreUtil.js
+++ packages/frontend/src/store/utils/StoreUtil.js
@@ -14,12 +14,12 @@ export default class StoreUtil {
       },
     };
   }
+
   /**
    * @param {Object} state - the state to update
    * @param {Object,Error} data - data to update with
    * @returns {Object} state after update
    */
-
   static updateState(state, data = undefined) {
     if (!state) throw new Error('state object is missing');
 
@@ -40,11 +40,54 @@ export default class StoreUtil {
   }
 
   /**
+   * Unlocks state and clears error depending on success parameter
+   * @param {Object} state - the state to update
+   * @param {Boolean} success - set to success state and clear error
+   * @returns {Object} state after update
+   */
+  static unlockState(state, success) {
+    return this._mutationUnlock({ ...state }, success);
+  }
+
+  /**
+   * Locks state without resetting stored data
+   * @param {Object} state - the state to update
+   * @returns {Object} state after update
+   */
+  static lockState(state) {
+    return this._mutationLock({ ...state });
+  }
+
+  /**
+   * @param {Object} state - the state to be unlocked
+   * @param {Boolean} success - set to success state
+   * @returns {Object} updated state
+   */
+  static _mutationUnlock(state, success) {
+    state.status.pending = false;
+    state.status.success = success;
+    state.status.fail = !success;
+    if (!success) state.status.error = null;
+    return state;
+  }
+
+  /**
+   * @param {Object} state - the state to be locked
+   * @returns {Object} updated state
+   */
+  static _mutationLock(state) {
+    state.status.pending = true;
+    state.status.success = false;
+    state.status.fail = false;
+    return state;
+  }
+
+  /**
    * @param {Object} state - the status to be put in pending state
    * @returns {Object} updated state
    */
   static _mutationPending(state) {
-    // state.data = null;
+    state.data = null;
     state.status.pending = true;
     state.status.success = false;
     state.status.fail = false;
