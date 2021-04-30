01639b08f3a0dfcba35f4db2f3f10b8cb16fe7bc
Philipp <dev@p-fruck.de>
2021-04-30 18:07:52 +0200

    init i18n

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -4,4 +4,13 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
 };
