fe03fdc7b6b624949265d407bb42c1389572bdb4
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-05-14 13:59:56 +0000

    👌 Removed todo comment

--- packages/backend/src/controllers/connectors/schemas/connectorLogItem.schema.ts
+++ packages/backend/src/controllers/connectors/schemas/connectorLogItem.schema.ts
@@ -12,12 +12,11 @@ export interface IConnectorLogItemDocument extends Document {
   type: ConnectorType;
 }
 
-// TODO: Add expire date to envvar!
-const connectorLogSchema = new Schema({
+const connectorLogItemSchema = new Schema({
   connector: { type: Schema.Types.ObjectId, ref: 'Connector', required: true },
   createdAt: { type: Date, default: Date.now, expires: config.connectorLogLifetime, },
   message: { type: String, required: true },
   type: { type: ConnectorLogType, required: true },
 });
 
-export const ConnectorLogItem: Model<IConnectorLogItemDocument> = model<IConnectorLogItemDocument>('connectorlog', connectorLogSchema);
+export const ConnectorLogItem: Model<IConnectorLogItemDocument> = model<IConnectorLogItemDocument>('connectorlog', connectorLogItemSchema);
