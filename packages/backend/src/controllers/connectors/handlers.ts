d5f932e37a2039919b8f41867b7cbf06a8212a6c
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-07-08 16:45:33 +0000

    ✨ Added PATCH /connectors/id

--- packages/backend/src/controllers/connectors/handlers.ts
+++ packages/backend/src/controllers/connectors/handlers.ts
@@ -1,13 +1,23 @@
+import { any, array, boolean, number, object, string } from '@hapi/joi';
 import { NextFunction, Request, Response } from 'express';
 import { loggerFile } from '../../configuration/logger';
-import { ApiSuccess } from '../../utils/api';
+import { ApiError, ApiSuccess } from '../../utils/api';
 import { connectorService } from './service';
 
 /****************************************
  *       User input validation          *
  * **************************************/
+const connectorsIdSchema = object({
+  id: string().hex().length(24).required(),
+});
 
-
+const connectorsIdPatchRequestSchema = object({
+  active: boolean(),
+  courses: array().items(number()),
+  default: boolean(),
+  name: string().max(64),
+  socket: object().unknown(),
+}).required();
 
 
 /****************************************
@@ -31,5 +41,26 @@ export function connectorsGetRequest(req: Request, res: Response, next: NextFunc
     loggerFile.error(err.message);
     next(err);
   }
+}
 
+export async function connectorsIdPatchRequest(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    // 1. Validate given id
+    const connectorIdValidation = connectorsIdSchema.validate(req.params);
+    if (connectorIdValidation.error) throw new ApiError(400, connectorIdValidation.error.message);
+
+    // 2. Validate request body
+    const connectorsIdPatchRequestValidation = connectorsIdPatchRequestSchema.validate(req.body);
+    if (connectorsIdPatchRequestValidation.error) throw new ApiError(400, connectorsIdPatchRequestValidation.error.message);
+
+    // 3. Update connector
+    const updatedConnector = await connectorService.update(connectorIdValidation.value.id, connectorsIdPatchRequestValidation.value);
+
+    // 4. Send response
+    const response = new ApiSuccess(200, updatedConnector);
+    next(response);
+  } catch (error) {
+    loggerFile.error(error.message);
+    next(error);
+  }
 }
--- packages/backend/src/routes/connectors.routes.ts
+++ packages/backend/src/routes/connectors.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { connectorsGetRequest } from '../controllers/connectors/handlers';
+import { connectorsGetRequest, connectorsIdPatchRequest } from '../controllers/connectors/handlers';
 export const connectorsRoutes = Router();
 
 // register routes
 connectorsRoutes.get('/connectors', connectorsGetRequest);
 connectorsRoutes.post('/connectors');
 connectorsRoutes.post('/connectors/:id');
-connectorsRoutes.patch('/connectors/:id');
+connectorsRoutes.patch('/connectors/:id', connectorsIdPatchRequest);
 connectorsRoutes.delete('/connectors/:id');
 connectorsRoutes.get('/connectors/:id/logs');
