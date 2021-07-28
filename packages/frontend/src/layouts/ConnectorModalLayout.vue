c34ab2136a94f9c1566617af55adbb3d05c932d4
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-07-28 19:37:51 +0000

    🚧 Added new action and modal layout

---
+++ packages/frontend/src/layouts/ConnectorModalLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-modal @close="onClose" :active.sync="active" :can-cancel.sync="canClose" :width="720" scroll="keep">
+    <div class="box">
+      <h1 class="title">{{title}}</h1>
+      <slot />
+    </div>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: 'ConnectorModalLayout',
+  methods: {
+    onClose() {
+      this.$emit('close');
+    },
+  },
+  props: {
+    title: String,
+    active: Boolean,
+    canClose: Boolean,
+  },
+};
+</script>
+
+<style>
+</style>
