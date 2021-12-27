833395e08c693a37929644e8024cdd6ffb718eac
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-12-27 10:21:56 +0000

    ♻ Applied new abstract class for messages

--- packages/backend/src/controllers/messages/templates/assignmentMessage.class.ts
+++ packages/backend/src/controllers/messages/templates/assignmentMessage.class.ts
@@ -1,13 +1,21 @@
-import { MessageTemplate } from '../message.class';
+import { Message } from '../message.class';
 
-export interface AssignmentMessageOptions {
-  course: string;
-  title: string;
-  dueDate: string;
-}
+export class AssignmentMessage extends Message {
 
-export class AssignmentMessage extends MessageTemplate {
-  readonly template = `**{course} - Neue Abgabe hinzugefügt!**
-  :information_source: "{title}" ist abzugeben
-  :alarm_clock: bis \`{dueDate}\``;
+  protected markdownTemplate = {
+    "EN": `**{{course}} - New submission added!**
+    :information_source: "{{title}}" is to be submitted
+    :alarm_clock: until \`{{dueDate}}\``,
+
+    "DE": `**{{course}} - Neue Abgabe hinzugefügt!**
+    :information_source: "{{title}}" ist abzugeben
+    :alarm_clock: bis \`{{dueDate}}\``
+  }
+
+  protected context: any;
+
+  constructor(course: string, title: string, dueDate: string) {
+    super();
+    this.context = { course, title, dueDate };
+  }
 }
--- packages/backend/src/controllers/messages/templates/resourceMessage.class.ts
+++ packages/backend/src/controllers/messages/templates/resourceMessage.class.ts
@@ -1,13 +1,21 @@
-import { MessageTemplate } from '../message.class';
+import { Message } from '../message.class';
 
-export interface ResourceMessageOptions {
-  course: string;
-  title: string;
-  link: string;
-}
+export class ResourceMessage extends Message {
 
-export class ResourceMessage extends MessageTemplate {
-  readonly template = `**{course} - Neue Datei**:
-  :file_folder: {title}
-  :link: {link}`;
+  protected markdownTemplate = {
+    "EN": `**{{course}} - New File**:
+    :file_folder: {{title}}
+    :link: {{link}}`,
+
+    "DE": `**{{course}} - Neue Datei**:
+    :file_folder: {{title}}
+    :link: {{link}}`
+  }
+
+  protected context: any;
+
+  constructor(course: string, title: string, link: string) {
+    super();
+    this.context = { course, title, link };
+  }
 }
