d4932f03dad59436f5c00c96e354642545112125
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-12-29 09:15:57 +0000

    🔒 Fixed missing auth validation

--- packages/backend/src/routes/connectors.routes.ts
+++ packages/backend/src/routes/connectors.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { isAuth } from '../controllers/authentication';
 import { connectorsGetRequest, connectorsIdDeleteRequest, connectorsIdPatchRequest, connectorsPostRequest } from '../controllers/connectors/handlers';
 export const connectorsRoutes = Router();
 
 // register routes
+connectorsRoutes.use(isAuth);
 connectorsRoutes.get('/connectors', connectorsGetRequest);
 connectorsRoutes.post('/connectors', connectorsPostRequest);
 connectorsRoutes.patch('/connectors/:id', connectorsIdPatchRequest);
