54def2bdab52f8e81c5d198dfda4aef26cbb2e8c
tjarbo <16938041+tjarbo@users.noreply.github.com>
2022-01-06 08:27:07 +0000

    :truck: Renamed LOGS_TO_FILE to LOG_TO_FILE

--- packages/backend/src/configuration/environment.ts
+++ packages/backend/src/configuration/environment.ts
@@ -19,7 +19,7 @@ const envVarsSchema = object({
   JWT_EXPIRESIN: string()
     .default('10m')
     .description('Defines how long a user will be logged in'),
-  LOGS_TO_FILE: boolean()
+  LOG_TO_FILE: boolean()
     .default(false)
     .description('Defines whether logs are written to filesystem or not'),
   MONGO_HOST: string()
@@ -91,7 +91,7 @@ export const config = {
     secret: envVars.JWT_SECRET,
     expiresIn: envVars.JWT_EXPIRESIN,
   },
-  logsToFile: envVars.LOGS_TO_FILE,
+  logToFile: envVars.LOG_TO_FILE,
   mongo: {
     host: envVars.MONGO_HOST,
   },
